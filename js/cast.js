a194cfff0260af116cf58feddd472b70e7b10ec0
moaaz-bhnas <moaaz_bs@yahoo.com>
2021-07-12 07:20:09 +0200

    https instead of http

--- js/cast.js
+++ js/cast.js
@@ -1,21 +1,19 @@
-const key ='VgIZq212Plt0KOArH5H8Cdqwr3SfsjpA';
+const key = "VgIZq212Plt0KOArH5H8Cdqwr3SfsjpA";
 
-const getWeather = async (id) =>{
-   const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-   const qeury = `${id}?apikey=${key}`;
+const getWeather = async (id) => {
+  const base = "https://dataservice.accuweather.com/currentconditions/v1/";
+  const qeury = `${id}?apikey=${key}`;
 
-   const response =  await fetch(base + qeury);
-   const data = await response.json();
-   return data[0];
+  const response = await fetch(base + qeury);
+  const data = await response.json();
+  return data[0];
 };
 
-// city information 
- const getCity = async (city) =>{
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const query = `?apikey=${key}&q=${city}`;
-    const response = await fetch(base + query);
-    const data = await response.json();
-    return data[0];
- };
-
- 
+// city information
+const getCity = async (city) => {
+  const base = "https://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
